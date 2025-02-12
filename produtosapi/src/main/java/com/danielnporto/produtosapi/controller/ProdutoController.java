dea382adb774f891cd7a26dda7ac29536ffe4b08
Daniel Porto <danielnporto10@gmail.com>
2025-02-12 12:54:43 -0300

    Add GET method to fetch product by ID

--- produtosapi/src/main/java/com/danielnporto/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/danielnporto/produtosapi/controller/ProdutoController.java
@@ -2,11 +2,9 @@ package com.danielnporto.produtosapi.controller;
 
 import com.danielnporto.produtosapi.model.Produto;
 import com.danielnporto.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -28,6 +26,11 @@ public class ProdutoController {
 
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id") String id){
+        return produtoRepository.findById(id).orElse(null);
     }
 
 }
